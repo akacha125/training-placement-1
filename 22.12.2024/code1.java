9ee2b7e9572ced3a19c7bf8ceb5c31293f65fa32
Dhinesha <147994787+Dhinesha@users.noreply.github.com>
2024-12-22 14:03:07 +0530

    code1.java

---
+++ 22.12.2024/code1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+        System.out.println("Sum: " + (a + b));
+    }
+}
