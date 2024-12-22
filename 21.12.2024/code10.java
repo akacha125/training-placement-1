3f171d019fde6d4abbce6f015e2eb6c90dda8b09
Dhinesha <147994787+Dhinesha@users.noreply.github.com>
2024-12-22 13:57:57 +0530

    Add files via upload

---
+++ 21.12.2024/code10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            
+                    // Read three integers from standard input
+                            int a = scanner.nextInt();
+                                    int b = scanner.nextInt();
+                                            int c = scanner.nextInt();
+                                            
+                                                    scanner.close(); // Close the scanner
+                                                    
+                                                            // Print each integer on a new line
+                                                                    System.out.println(a);
+                            
