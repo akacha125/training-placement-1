5c4e7f98f397f31cfa39b31fcf8860841f1b0daa
Dhinesha <147994787+Dhinesha@users.noreply.github.com>
2024-12-22 14:11:28 +0530

    code8.java

---
+++ 22.12.2024/code8.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        int original = num, reversed = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        if (original == reversed) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
