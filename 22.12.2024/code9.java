406de0a287e0385d06ab95900bbd092d0e1614c4
Dhinesha <147994787+Dhinesha@users.noreply.github.com>
2024-12-22 14:12:11 +0530

    code9.java

---
+++ 22.12.2024/code9.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double a = scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        double b = scanner.nextDouble();
+        System.out.print("Enter operation (+, -, *, /): ");
+        char op = scanner.next().charAt(0);
+
+        switch (op) {
+            case '+':
+                System.out.println("Result: " + (a + b));
+                break;
+            case '-':
+                System.out.println("Result: " + (a - b));
+                break;
+            case '*':
+                System.out.println("Result: " + (a * b));
+                break;
+            case '/':
+                if (b != 0) {
+                    System.out.println("Result: " + (a / b));
+                } else {
+                    System.out.println("Cannot divide by zero");
+                }
+                break;
+            default:
+                System.out.println("Invalid operation");
+        }
+    }
+}
