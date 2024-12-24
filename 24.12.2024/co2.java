a876e8540cc5c68e8279cbfd21e9f5a23d290c49
Dhinesha <147994787+Dhinesha@users.noreply.github.com>
2024-12-24 17:23:47 +0530

    co2.java

---
+++ 24.12.2024/co2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("Reversed array:");
+        for (int i = size - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
